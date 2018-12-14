2b8ce1f9d629324c8f1ad775a6e7137353525371
Swig-DPI <scott.mwigle@gmail.com>
2018-12-14 15:36:34 -0700

    fix mistake in file again again

--- xfr_chris_l/Transfer_model.py
+++ xfr_chris_l/Transfer_model.py
@@ -398,11 +398,10 @@ if __name__ == '__main__':
                                        use_multiprocessing=True,
                                        verbose=1)
     print(f"holdout loss: {metrics[0]} accuracy: {metrics[1]}")
-    train_generator.reset()
-    validation_generator.reset()
+
 
     ### Hold out set predictions to CSV
-    predictions = model.predict_generator(self.holdout_generator, steps = transfer_cnn.num_holdout/transfer_cnn.batch_size)
+    predictions = model.predict_generator(transfer_cnn.holdout_generator, steps = transfer_cnn.num_holdout/transfer_cnn.batch_size)
     pred_vals = predictions
     vec = np.vectorize(lambda x: 1 if x>0.6 else 0)
     predicted_class_indices=vec(predictions)
@@ -416,7 +415,7 @@ if __name__ == '__main__':
     results.to_csv("transfer_learning_holdout_set.csv",index=False)
 
     ###  Test set predictions to CSV
-    predictions = model.predict_generator(self.validation_generator, steps = transfer_cnn.num_val/transfer_cnn.batch_size)
+    predictions = model.predict_generator(transfer_cnn.validation_generator, steps = transfer_cnn.num_val/transfer_cnn.batch_size)
     pred_vals = predictions
     vec = np.vectorize(lambda x: 1 if x>0.6 else 0)
     predicted_class_indices=vec(predictions)
@@ -430,14 +429,14 @@ if __name__ == '__main__':
     results.to_csv("transfer_learning_validation_set.csv",index=False)
 
     ###  Test set predictions to CSV
-    predictions = model.predict_generator(self.test_generator, steps = transfer_cnn.num_test/transfer_cnn.batch_size)
+    predictions = model.predict_generator(transfer_cnn.train_generator, steps = transfer_cnn.num_test/transfer_cnn.batch_size)
     pred_vals = predictions
     vec = np.vectorize(lambda x: 1 if x>0.6 else 0)
     predicted_class_indices=vec(predictions)
-    labels = (transfer_cnn.test_generator.class_indices)
+    labels = (transfer_cnn.train_generator.class_indices)
     labels = dict((v,k) for k,v in labels.items())
     predictions = [labels[k] for k in predicted_class_indices.ravel()]
-    filenames=transfer_cnn.test_generator.filenames[:len(predictions)]
+    filenames=transfer_cnn.train_generator.filenames[:len(predictions)]
     results=pd.DataFrame({"Filename":filenames,
                           "Predictions":predictions,
                           "Values":pred_vals.ravel()})
